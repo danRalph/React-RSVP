b613be6158491de07d5fc9057486a3fcf4726b71
danRalph <dan.r.richards@hotmail.co.uk>
2020-06-02 13:26:49 +0100

    Updates

---
+++ precache-manifest.5e3bcd2834302f80fee1e4bf2772b441.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b7a755df83ce8abdff47c611f5d4c9",
+    "url": "/React-RSVP/index.html"
+  },
+  {
+    "revision": "d814f5f3b08b1a18842f",
+    "url": "/React-RSVP/static/css/main.73197f2c.chunk.css"
+  },
+  {
+    "revision": "7aa2512552ea9efec654",
+    "url": "/React-RSVP/static/js/2.f0021a35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-RSVP/static/js/2.f0021a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d814f5f3b08b1a18842f",
+    "url": "/React-RSVP/static/js/main.e1d5d897.chunk.js"
+  },
+  {
+    "revision": "c6c9b451bbd79f4ad342",
+    "url": "/React-RSVP/static/js/runtime-main.7333fd10.js"
+  },
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/React-RSVP/static/media/header-bg.be92ab00.jpg"
+  }
+]);
